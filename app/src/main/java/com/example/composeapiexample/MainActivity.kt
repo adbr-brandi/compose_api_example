80f96766b39a6ae73a9f02de60907abda1895d0b
bora <andbr284@brandi.co.kr>
2023-04-20 17:19:41 +0900

    feat: (WIP) SearchPage Style to Top

--- app/src/main/java/com/example/composeapiexample/MainActivity.kt
+++ app/src/main/java/com/example/composeapiexample/MainActivity.kt
@@ -5,8 +5,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -16,10 +19,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.composeapiexample.data.data_source.searchImage
 import com.example.composeapiexample.data.model.Document
+import com.example.composeapiexample.ui.theme.Black500
 import com.example.composeapiexample.ui.theme.ComposeAPIExampleTheme
+import com.example.composeapiexample.ui.theme.LightGrey100
 
 class MainActivity : ComponentActivity() {
     @SuppressLint("UnrememberedMutableState")
@@ -61,28 +69,69 @@ fun SearchPage(
     results: List<Document>,
 ) {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(20.dp)
+        modifier = Modifier.fillMaxSize()
+
     ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            OutlinedTextField(value = query, onValueChange = setQuery)
-            IconButton(onClick = onSearch) {
-                Icon(imageVector = Icons.Default.Search, contentDescription = "")
-            }
-        }
-        Spacer(modifier = Modifier.size(20.dp))
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-            contentPadding = PaddingValues(horizontal = 4.dp, vertical = 10.dp),
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(horizontal = 20.dp, vertical = 10.dp)
+
         ) {
-            items(items = results) {
-                Card(modifier = Modifier.fillMaxWidth(), elevation = 10.dp) {
-                    Row() {
-                        Text(it.imageURL)
-                    }
+            Box(
+                Modifier.weight(6f)
+            ) {
+                OutlinedTextField(
+                    value = query,
+                    onValueChange = setQuery,
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.textFieldColors(
+                        backgroundColor = LightGrey100,
+                        disabledTextColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        disabledIndicatorColor = Color.Transparent
+                    ),
+                    shape = RoundedCornerShape(8.dp),
+                )
+            }
+            Box(
+                Modifier.weight(1f)
+            ) {
+                Button(
+                    onClick = onSearch,
+                    contentPadding = PaddingValues(0.dp),
+                    shape = CircleShape,
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = Black500
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
                 }
+            }
+
+
+        }
+        Divider()
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 10.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalArrangement = Arrangement.spacedBy(4.dp),
+        ) {
+            items(results) {
+                AsyncImage(
+                    model = it.imageURL,
+                    contentDescription = it.displaySitename,
+                    contentScale = ContentScale.Fit,
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
