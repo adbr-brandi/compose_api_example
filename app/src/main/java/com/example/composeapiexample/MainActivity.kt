442999d0a2d28b4fd1f2ad88ec7ee5e34801a692
bora <andbr284@brandi.co.kr>
2023-04-24 17:24:42 +0900

    refactor: 기존 viewmodel 호출 방식 -> compose 내 재호출 가능한 viewmodel 형식으로 변경

--- app/src/main/java/com/example/composeapiexample/MainActivity.kt
+++ app/src/main/java/com/example/composeapiexample/MainActivity.kt
@@ -3,22 +3,23 @@ package com.example.composeapiexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.composeapiexample.data.data_source.searchImage
 import com.example.composeapiexample.ui.main.SearchPage
 import com.example.composeapiexample.ui.main.SearchPageViewModel
 import com.example.composeapiexample.ui.theme.ComposeAPIExampleTheme
 
 class MainActivity : ComponentActivity() {
-    private val vm by viewModels<SearchPageViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val vm = viewModel<SearchPageViewModel>()
+
             ComposeAPIExampleTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
